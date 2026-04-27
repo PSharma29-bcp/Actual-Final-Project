9eb33e0fc86dc881444279a77132ade9f7e17a18
Sharma <p.sharma29@bcp.org>
2026-04-27 12:30:51 -0700

    first commit

---
+++ Final Project.py
@@ -0,0 +1,28 @@
+import sys, pygame
+import random
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+
+# Screen Dimensions
+WIDTH = 800
+HEIGHT = 400
+
+# Constants
+
+
+FPS = 30 # Frames per second (used with clock)
+
+# RGB Values of screen objects
+WOOD_COLOR = (179, 104, 9)
+TABLE_COLOR = (112, 240, 112)
+# Yellow, Blue, Red, Purple, Orange, Green, Maroon
+
+
+
+
+
+
+
+
+
+
